d8197e43a2709dfe76a7747f6ff8baee6e004d95
Ayaan <syedayaan9376@gmail.com>
2024-09-11 01:28:11 +0530

    recyclerview fetching data from firestore friends collection

---
+++ app/src/main/java/eu/tutorials/sos/ui/slideshow/MyAdapter.java
@@ -0,0 +1,59 @@
+package eu.tutorials.sos.ui.slideshow;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import eu.tutorials.sos.R;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<friends> contactsList;
+
+    // Constructor
+    public MyAdapter(Context context, ArrayList<friends> contactsList) {
+        this.context = context;
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the contact item layout
+        View v = LayoutInflater.from(context).inflate(R.layout.contacts, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // Get the current friend item
+        friends friend = contactsList.get(position);
+
+        // Set the name and phone using getter methods
+        holder.Name.setText(friend.getName());
+        holder.Phone.setText(friend.getPhone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactsList.size();
+    }
+
+    // ViewHolder class to bind the view items
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView Name, Phone;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Name = itemView.findViewById(R.id.tvName);
+            Phone = itemView.findViewById(R.id.tvPhone);
+        }
+    }
+}
--- app/src/main/java/eu/tutorials/sos/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/eu/tutorials/sos/ui/slideshow/SlideshowFragment.java
@@ -1,21 +1,42 @@
 package eu.tutorials.sos.ui.slideshow;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import eu.tutorials.sos.R;
 import eu.tutorials.sos.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
-
+    RecyclerView recyclerView;
+    ArrayList<friends> contactsList;
+    MyAdapter MyAdapter;
+    FirebaseFirestore db;
+    ProgressDialog pd;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SlideshowViewModel slideshowViewModel =
@@ -23,11 +44,66 @@ public class SlideshowFragment extends Fragment {
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        pd=new ProgressDialog(getContext());
+        pd.setCancelable(false);
+        pd.setMessage("Fetching Data...");
+        pd.show();
+        recyclerView=root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        db=FirebaseFirestore.getInstance();
+        contactsList=new ArrayList<friends>();
+        MyAdapter=new MyAdapter(getContext(),contactsList);
+        recyclerView.setAdapter(MyAdapter);
+        MyAdapter.notifyDataSetChanged();
+        EventChangeListener();
         return root;
     }
+
+    private void EventChangeListener() {
+        Log.d("Firestore Data", "Fetching data...");
+
+        db.collection("friends")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        Log.d("Firestore Data", "Fetching data...");
+                        if (error != null) {
+                            if (pd.isShowing()) {
+                                pd.dismiss();
+                            }
+                            Log.i("Firestore Exception", error.getMessage());
+                            return;
+                        }
+
+                        if (value == null) {
+                            Log.i("Firestore Data", "No data available");
+                            return;
+                        }
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                String id = dc.getDocument().getId();
+                                Log.i("Firestore Data", "Document ID: " + id);
+                                Map<String, Object> friendMap = dc.getDocument().getData();
+                                Log.i("Firestore Data", "Document Data: " + friendMap.toString());
+
+                                String phone = (String) friendMap.get("Phone");
+                                String name = (String) friendMap.get("name");
+                                contactsList.add(new friends(phone, name));
+                                Log.d("Firestore Data", "Name: " + name + ", Phone: " + phone);
+                            }
+                        }
+                        MyAdapter.notifyDataSetChanged();
+                        if (pd.isShowing()) {
+                            pd.dismiss();
+                        }
+                    }
+                });
+    }
+
+
+
 
     @Override
     public void onDestroyView() {
