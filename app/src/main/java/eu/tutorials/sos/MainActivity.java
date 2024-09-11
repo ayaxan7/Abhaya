456bf3fe5867d34c1c587d3d0b8a94d087f63290
Ayaan <syedayaan9376@gmail.com>
2024-09-11 21:41:30 +0530

    updating firestore with logged in user's uid and fcm token of friend

--- app/src/main/java/eu/tutorials/sos/MainActivity.java
+++ app/src/main/java/eu/tutorials/sos/MainActivity.java
@@ -16,7 +16,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
